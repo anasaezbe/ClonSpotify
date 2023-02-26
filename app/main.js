92670b02266f0beaa4676240f52954153e2a9386
Ana Sáez-Benito <saezbenitoana@gmail.com>
2023-02-26 16:58:38 +0100

    first commit

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import "/assets/sass/main.scss"
+import "/assets/sass/partials/landing.scss"
+import "/assets/sass/partials/fixedelements.scss"
+import "/assets/sass/partials/playlist.scss"
+import "/assets/sass/partials/artist.scss"
